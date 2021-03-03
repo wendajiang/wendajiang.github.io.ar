ab0b2fcbe13fd91b59d2e10420a89582f4c9ac55
wendajiang <wendajiang93@163.com>
2021-03-04 02:58:20 +0800

    update rust tool

--- genmd_rust/src/main.rs
+++ genmd_rust/src/main.rs
@@ -1,26 +1,37 @@
 use std::{env, fmt, io::Write, process::exit};
-// use std::fs;
 use std::time::{SystemTime};
 use chrono::offset::Utc;
 use chrono::DateTime;
 use std::fs::OpenOptions;
 use chrono::prelude::*;
+use clap::{Arg, App};
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
-    // println!("{}", args.len());
-    if args.len() < 2 {
-        println!("Usage: genmd file_name [-p path]");
-        exit(0);
-    }
+    let matches = App::new("genmd")
+        .version("1.0")
+        .author("david")
+        .about("generate md file")
+        .arg(Arg::new("file_path")
+            .short('p')
+            .long("file_path")
+            .value_name("FILE_PATH")
+            .about("get the file path, example ./help")
+            .takes_value(true))
+        .arg(Arg::new("title")
+            .short('t')
+            .long("title")
+            .value_name("TITLE")
+            .takes_value(true)
+            .about("get the blog title"))
+        .arg(Arg::new("file_name")
+            .required(true)
+            .index(1))
+        .get_matches();
 
-    let mut file_path = "./_posts/";
-    if args.len() == 4 {
-        file_path = &args[3];
-        println!("-p file_path is {}", file_path);
-    }
+    let file_path = matches.value_of("file_path").unwrap_or("./_posts/");
+    let title = matches.value_of("title").unwrap_or("");
+    let file_name_in = matches.value_of("file_name").unwrap();
 
     let pre_content = "---\nlayout: posts\ndate: ";
 
@@ -32,10 +43,10 @@ fn main() {
     let str_datetime = local_time.format("%Y-%m-%d %T");
     let file_prefix = local_time.format("%Y-%m-%d");
 
-    let file_name = std::format!("{}-{}.md", file_prefix, &args[1]);
+    let file_name = std::format!("{}-{}.md", file_prefix, file_name_in);
     println!("file_name is {}", file_name);
 
-    let content = std::format!("{}{}\ncategories:\ntitle:\n---\n", pre_content, str_datetime);
+    let content = std::format!("{}{} GMT\ncategories:\ntitle:{}\n---\n", pre_content, str_datetime, title);
     println!("{}", content);
 
     let full_file_name = std::format!("{}{}", file_path, file_name);
