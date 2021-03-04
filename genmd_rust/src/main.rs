3d54e8b182fce0b7e87958045d98f29ffa169428
johnwdjiang <johnwdjiang@tencent.com>
2021-03-04 12:22:05 +0800

    upgrade support mermaid

--- genmd_rust/src/main.rs
+++ genmd_rust/src/main.rs
@@ -1,8 +1,5 @@
-use std::{env, fmt, io::Write, process::exit};
-use std::time::{SystemTime};
-use chrono::offset::Utc;
+use std::{io::Write};
 use chrono::DateTime;
-use std::fs::OpenOptions;
 use chrono::prelude::*;
 use clap::{Arg, App};
 
@@ -17,25 +14,39 @@ fn main() {
             .long("file_path")
             .value_name("FILE_PATH")
             .about("get the file path, example ./help")
-            .takes_value(true))
+            .takes_value(true)
+            )
         .arg(Arg::new("title")
             .short('t')
             .long("title")
             .value_name("TITLE")
             .takes_value(true)
-            .about("get the blog title"))
+            .about("get the blog title")
+            )
+        .arg(Arg::new("mermaid")
+            .short('m')
+            .long("mermaid")
+            .value_name("mermaid on/off")
+            .about("-m/--mermaid true to turn on mermaid")
+            )
         .arg(Arg::new("file_name")
             .required(true)
             .index(1))
         .get_matches();
-
+    
     let file_path = matches.value_of("file_path").unwrap_or("./_posts/");
     let title = matches.value_of("title").unwrap_or("");
     let file_name_in = matches.value_of("file_name").unwrap();
+    let mermaid_flag = matches.value_of("mermaid").unwrap_or("false");
+    let mut mermaid_template = "<!--\nmermaid example:\n<div class=\"mermaid\"\n    mermaid program\n</div>\n-->";
+    if mermaid_flag == "false" {
+        mermaid_template = "";
+    }
+    
 
-    let pre_content = "---\nlayout: posts\ndate: ";
+    let pre_content = "---\nlayout: post\ndate: ";
 
-    let utc_time: DateTime<Utc> = Utc::now();       
+    // let utc_time: DateTime<Utc> = Utc::now();       
     let local_time: DateTime<Local> = Local::now(); 
 
     // println!("{}", utc_time.format("%Y-%m-%d %T"));
@@ -46,7 +57,8 @@ fn main() {
     let file_name = std::format!("{}{}-{}.md", file_path, file_prefix, file_name_in);
     println!("file_name is {}", file_name);
 
-    let content = std::format!("{}{} GMT\ncategories:\ntitle: \"{}\"\n---\n", pre_content, str_datetime, title);
+    let content = std::format!("{}{} +0800\ncategories:\ntitle: \"{}\"\nmermaid: {}\n---\n{}", pre_content, str_datetime, title, 
+                mermaid_flag, mermaid_template);
     println!("{}", content);
 
     let mut file = std::fs::OpenOptions::new()
