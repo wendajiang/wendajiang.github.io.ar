e5b0f29941782ccb6b64165956ca0fea51db44ef
wendajiang <wendajiang93@163.com>
2021-12-08 15:52:39 +0800

    fmt

--- genmd_rust/src/main.rs
+++ genmd_rust/src/main.rs
@@ -1,48 +1,48 @@
-use std::{io::Write};
-use chrono::DateTime;
 use chrono::prelude::*;
-use clap::{Arg, App};
-
+use chrono::DateTime;
+use clap::{App, Arg};
+use std::io::Write;
 
 fn main() {
     let matches = App::new("genmd")
         .version("1.0")
         .author("david")
         .about("generate md file")
-        .arg(Arg::new("file_path")
-            .short('p')
-            .long("file_path")
-            .value_name("FILE_PATH")
-            .about("get the file path, example ./help")
-            .takes_value(true)
-            )
-        .arg(Arg::new("title")
-            .short('t')
-            .long("title")
-            .value_name("TITLE")
-            .takes_value(true)
-            .about("get the blog title")
-            )
-        .arg(Arg::new("mermaid")
-            .short('m')
-            .long("mermaid")
-            .value_name("mermaid true/false")
-            .about("-m/--mermaid true to turn on mermaid")
-            )
-        .arg(Arg::new("file_name")
-            .required(true)
-            .index(1))
+        .arg(
+            Arg::new("file_path")
+                .short('p')
+                .long("file_path")
+                .value_name("FILE_PATH")
+                .about("get the file path, example ./help")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("title")
+                .short('t')
+                .long("title")
+                .value_name("TITLE")
+                .takes_value(true)
+                .about("get the blog title"),
+        )
+        .arg(
+            Arg::new("mermaid")
+                .short('m')
+                .long("mermaid")
+                .value_name("mermaid true/false")
+                .about("-m/--mermaid true to turn on mermaid"),
+        )
+        .arg(Arg::new("file_name").required(true).index(1))
         .get_matches();
 
     let file_path = matches.value_of("file_path").unwrap_or("./content/");
     let title = matches.value_of("title").unwrap_or("");
     let file_name_in = matches.value_of("file_name").unwrap();
     let mermaid_flag = matches.value_of("mermaid").unwrap_or("false");
-    let mut mermaid_template = "<!--\nmermaid example:\n<div class=\"mermaid\">\n    mermaid program\n</div>\n-->";
+    let mut mermaid_template =
+        "<!--\nmermaid example:\n<div class=\"mermaid\">\n    mermaid program\n</div>\n-->";
     if mermaid_flag == "false" {
         mermaid_template = "";
     }
-
 
     let pre_content = "+++\ntemplate = \"page.html\"\n";
 
@@ -60,11 +60,11 @@ fn main() {
     println!("{}", content);
 
     let mut file = std::fs::OpenOptions::new()
-                                            .read(true)
-                                            .write(true)
-                                            .create(true)
-                                            .open(file_name).unwrap();
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(file_name)
+        .unwrap();
 
     file.write_fmt(format_args!("{}", content)).unwrap();
-
 }
